63e6af798d7e9a70f3e3e98ab65fec6f260a6c60
GulomjonBakirov <bakirov.dev@gmail.com>
2023-04-04 11:37:16 +0500

    refactor: Typeorm Repository

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -19,7 +19,7 @@ export class CoffeesController {
   @Get(':id')
   findOne(@Param('id') id: number) {
     console.log(typeof id)
-    return this.coffeesService.findOne('' + id)
+    return this.coffeesService.findOne(id)
   }
 
   @Post()
@@ -30,12 +30,12 @@ export class CoffeesController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+  update(@Param('id') id: number, @Body() updateCoffeeDto: UpdateCoffeeDto) {
     return this.coffeesService.update(id, updateCoffeeDto)
   }
 
   @Delete(':id')
-  delete(@Param('id') id: string) {
+  delete(@Param('id') id: number) {
     return this.coffeesService.remove(id)
   }
 }
